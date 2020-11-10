b26894a053d2f0f6860ae378d5d90f391ecda6ee
shenni <shenni@softia.fr>
2020-11-10 11:25:22 +0100

    :wrench: changed delimiter to origin $

--- fsutils/csv_manager.py
+++ fsutils/csv_manager.py
@@ -5,7 +5,7 @@ import re
 def init_csv():
     with open('./articles.csv', 'w') as csvfile:
         # original delimiter was $
-        spamwriter = csv.writer(csvfile, delimiter=';',
+        spamwriter = csv.writer(csvfile, delimiter='$',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow(["title", "summary", "content", "tags", "date", "image_url", "image_alt",
                              "image_title", "url_tag", "language"])
@@ -20,7 +20,7 @@ def init_csv():
 def create_csv(title, summary, content, tags, date, image_url, image_alt, image_title, url_tag, language,
                categories):
     with open('./articles.csv', 'a') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=';',
+        spamwriter = csv.writer(csvfile, delimiter='$',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow([title, summary, content, tags, date, image_url, image_alt, image_title, url_tag, language])
 
